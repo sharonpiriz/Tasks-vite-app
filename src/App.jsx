a3872ef7b23658ecfd1e463b31100505a85b3bba
sharonpiriz <sharonacunha12@hotmail.com>
2022-08-19 01:53:51 -0300

    height fix on app component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import TaskForm from "./Components/TaskForm";
 
 const App = () => {
   return (
-    <main className="bg-zinc-900 h-screen">
+    <main className="bg-zinc-900 h-full">
       <div className="container mx-auto p-10">
         <TaskForm />
         <TaskList />
