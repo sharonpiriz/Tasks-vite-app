33ac7a05b5d4dd038af412c7fb63e2deb6779516
sharonpiriz <sharonacunha12@hotmail.com>
2022-08-19 02:00:04 -0300

    heigth fix h-screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import TaskForm from "./Components/TaskForm";
 const App = () => {
   return (
     <main className="bg-zinc-900 h-full">
-      <div className="container mx-auto p-10">
-        <TaskForm />
-        <TaskList />
+      <div className="bg-zinc-900 h-screen">
+        <div className="container mx-auto p-10">
+          <TaskForm />
+          <TaskList />
+        </div>
       </div>
     </main>
   );
